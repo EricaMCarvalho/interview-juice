e42e41125c7ff1a8332c051935c451d2d0c872a4
Erica Carvalho <ericamcar28@gmail.com>
2021-04-16 14:05:30 +0100

    connect database

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 
+const connectDB = require('./config/db');
 const decks = require('./routes/decks');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 const app = express();
 
@@ -19,9 +24,17 @@ app.use('/api/v1/decks', decks);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `Server is listening in ${process.env.NODE_ENV} on port ${process.env.PORT}`
+      .yellow.bold
   )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
