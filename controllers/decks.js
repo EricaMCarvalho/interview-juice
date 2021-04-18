2336538c04e33575c257e911be55b1e32b35deb6
Erica Carvalho <ericamcar28@gmail.com>
2021-04-18 22:30:20 +0100

    error handling

--- controllers/decks.js
+++ controllers/decks.js
@@ -1,79 +1,62 @@
 const Deck = require('../models/Deck');
+const AppError = require('../utils/AppError');
+const catchAsync = require('../middleware/catchAsync');
 
 // @desc    Get all decks
 // @route   GET /api/v1/decks
 // @access  Public
-exports.getDecks = async (req, res, next) => {
-  try {
-    const decks = await Deck.find();
-    res.status(200).json({ success: true, count: decks.length, data: decks });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+exports.getDecks = catchAsync(async (req, res, next) => {
+  const decks = await Deck.find();
+  res.status(200).json({ success: true, count: decks.length, data: decks });
+});
 
 // @desc    Get single deck
 // @route   GET /api/v1/decks/:id
 // @access  Public
-exports.getDeck = async (req, res, next) => {
-  try {
-    const deck = await Deck.findById(req.params.id);
+exports.getDeck = catchAsync(async (req, res, next) => {
+  const deck = await Deck.findById(req.params.id);
 
-    if (!deck) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({ success: true, data: deck });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!deck) {
+    // return res.status(400).json({ success: false });
+    return next(new AppError(`Deck not found with id ${req.params.id}`, 404));
   }
-};
+
+  res.status(200).json({ success: true, data: deck });
+});
 
 // @desc    Create deck
 // @route   POST /api/v1/decks
 // @access  Private
-exports.createDeck = async (req, res, next) => {
-  try {
-    const deck = await Deck.create(req.body);
-    res.status(201).json({ success: true, data: deck });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+exports.createDeck = catchAsync(async (req, res, next) => {
+  const deck = await Deck.create(req.body);
+  res.status(201).json({ success: true, data: deck });
+});
 
 // @desc    Update deck
 // @route   PUT /api/v1/decks/:id
 // @access  Private
-exports.updateDeck = async (req, res, next) => {
-  try {
-    const deck = await Deck.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+exports.updateDeck = catchAsync(async (req, res, next) => {
+  const deck = await Deck.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    if (!deck) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(201).json({ success: true, data: deck });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!deck) {
+    return next(new AppError(`Deck not found with id ${req.params.id}`, 404));
   }
-};
+
+  res.status(201).json({ success: true, data: deck });
+});
 
 // @desc    Delete deck
 // @route   DELETE /api/v1/decks/:id
 // @access  Private
-exports.deleteDeck = async (req, res, next) => {
-  try {
-    const deck = await Deck.findByIdAndRemove(req.params.id);
+exports.deleteDeck = catchAsync(async (req, res, next) => {
+  const deck = await Deck.findByIdAndRemove(req.params.id);
 
-    if (!deck) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(201).json({ success: true, data: {} });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!deck) {
+    return next(new AppError(`Deck not found with id ${req.params.id}`, 404));
   }
-};
+
+  res.status(201).json({ success: true, data: {} });
+});
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const colors = require('colors');
 
 const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
 const decks = require('./routes/decks');
 
 // Load env vars
@@ -24,6 +25,8 @@ if (process.env.NODE_ENV === 'development') {
 
 // Routes
 app.use('/api/v1/decks', decks);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
